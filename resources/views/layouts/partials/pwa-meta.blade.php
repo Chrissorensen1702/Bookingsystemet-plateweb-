efebde1e22c0a3c663924174d5da23f21a83b25d
Chris Sørensen <chrise.sorensen@MacBook-Air-tilhrende-Chris.local>
2026-03-30 03:47:43 +0200

    style: center/scale PWA icon and rename app to PlateBook

--- resources/views/layouts/partials/pwa-meta.blade.php
+++ resources/views/layouts/partials/pwa-meta.blade.php
@@ -2,7 +2,7 @@
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
-<meta name="apple-mobile-web-app-title" content="PlæBooking">
+<meta name="apple-mobile-web-app-title" content="PlateBook">
 <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/logo/pwa-192.png') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/logo/apple-touch-icon.png') }}">
